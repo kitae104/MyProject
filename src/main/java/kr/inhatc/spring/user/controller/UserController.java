a4ddd327db545c43f899964e5d2851a8eb67d0a7
kitae104 <aqua0405@gmail.com>
2020-05-21 04:21:28 +0900

    JPA

--- src/main/java/kr/inhatc/spring/user/controller/UserController.java
+++ src/main/java/kr/inhatc/spring/user/controller/UserController.java
@@ -7,7 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.inhatc.spring.board.dto.BoardDto;
 import kr.inhatc.spring.board.service.BoardService;
@@ -28,7 +31,8 @@ public class UserController {
 		return "redirect:/user/userList";
 	}
 	
-	@RequestMapping("/user/userList")
+	// GET(read), POST(create), PUT(update), DELETE(delete) 
+	@RequestMapping(value = "/user/userList", method=RequestMethod.GET)
 	public String userList(Model model) {		
 		List<Users> list = userService.userList();
 		model.addAttribute("list", list);
